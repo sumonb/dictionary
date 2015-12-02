e28a6a0e9594963b5215dc9d34bdc0947b9d36e2
sumonb <spyctg@yahoo.com>
2015-12-02 13:10:55 +1100

    first

---
+++ Dictionary/Dictionary/app/background.js
@@ -0,0 +1,33 @@
+﻿// A generic onclick callback function.
+function genericOnClick(info, tab) {
+    //https://www.google.com.au/search?q=define+good
+    //$.ajax({
+    //    url: "https://www.google.com.au/search?q=define+good", success: function (result) {
+    //        alert(result);
+    //    }
+    //});
+    
+    //console.log("info: " + JSON.stringify(info));
+    //var optionsUrl = chrome.extension.getURL('show.html');
+    chrome.tabs.create({ url: "https://www.google.com.au/search?q=define+" + info.selectionText + "&whatdoumean" });
+  
+
+}
+
+function createContextMenu() {
+    // Create one test item for each context type.
+    var contexts = ["selection"];
+    var title = "What does it mean?";
+
+    var id = chrome.contextMenus.create({
+        "title": title,
+        "contexts": contexts,
+        "onclick": genericOnClick
+    });
+
+}
+
+//Extenstion load event
+createContextMenu();
+
+
---
+++ Dictionary/Dictionary/app/show.js
@@ -0,0 +1,4 @@
+﻿$(function () {
+    alert('ss');
+    $("#containerExtResult").load("https://www.google.com.au/search?q=define+good #uid_0");
+});
