9777571a9433483c9a8795f405c16118bb6c19dd
sumonb <spyctg@yahoo.com>
2015-12-02 14:41:10 +1100

    .

--- Dictionary/Dictionary/app/background.js
+++ Dictionary/Dictionary/app/background.js
@@ -1,17 +1,6 @@
 ﻿// A generic onclick callback function.
 function genericOnClick(info, tab) {
-    //https://www.google.com.au/search?q=define+good
-    //$.ajax({
-    //    url: "https://www.google.com.au/search?q=define+good", success: function (result) {
-    //        alert(result);
-    //    }
-    //});
-    
-    //console.log("info: " + JSON.stringify(info));
-    //var optionsUrl = chrome.extension.getURL('show.html');
     chrome.tabs.create({ url: "https://www.google.com.au/search?q=define+" + info.selectionText + "&whatdoumean" });
-  
-
 }
 
 function createContextMenu() {
